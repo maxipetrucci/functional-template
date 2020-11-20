ef11ce2bb0fdc0c6c135cfc3fc86d708fc05787b
Maximiliano Petrucci <mpetrucci@fi.uba.ar>
2020-11-20 20:12:38 -0300

    Hecho el ejercicio 8

--- src/exercise8.clj
+++ src/exercise8.clj
@@ -3,13 +3,13 @@
 (defn squear 
   "Returns the squear of a given number"
   [x]
-  (throw (Exception. "Not implemented."))
+  (* x x)
 )
 
 (defn fderive
   "Returns a function that approximates the derive of f with error h in the value given to the returned function."
   [f h]
-  (throw (Exception. "Not implemented."))
+  (fn [p] (/ (- (f (+ p h)) (f (- p h))) (* 2 h)))
 )
 
 
